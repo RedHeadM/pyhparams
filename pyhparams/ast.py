de352595925ce605f37bf845f21d153ad8b1856c
markus <markus.merklinger@bbraun.com>
2023-05-17 14:29:24 +0200

    ast key and dataclass merge

--- pyhparams/ast.py
+++ pyhparams/ast.py
@@ -90,6 +90,8 @@ def _merge_dict(target: ast.Dict, base: ast.Dict) -> ast.Dict:
 
     return ast.Dict(keys =keys_merged,  values =values_merged)
 
+# def _merge_dict(target: ast.Dict, base: ast.Dict) -> ast.Dict:
+
 def _merge_assign_dict(target: ast.Assign, base: ast.Assign) -> ast.Assign:
     # check if both are expected dict type
     for s in (target, base):
@@ -98,15 +100,73 @@ def _merge_assign_dict(target: ast.Assign, base: ast.Assign) -> ast.Assign:
         assert isinstance(s.targets[0], ast.Name)
         # assert isinstance(s.value, ast.Dict)
 
-    assert isinstance(target.value, ast.Dict)
-    assert isinstance(base.value, ast.Dict)
+    assert isinstance(target.value, ast.Call)
+    assert isinstance(base.value, ast.Call)
     target.value= _merge_dict(target.value,base.value)
+    return target
+# {} Dict key[list[cost]] value
+# dict Call keywords=[keyword(arg='name', value=Constant(value='foo'))]
+
+# dc only kwargs keywords=[keyword(arg='name', value=Constant(value='foo'))]
+
+def _is_mergeable(target: ast.stmt, base: ast.stmt);
+    ''' if dict oder dataclass''' 
+
+def _merge_keyword(target: List[ast.keyword], base: List[ast.keyword]) -> List[ast.keyword]:
+    target_kw = {k.arg: k.value for k in target}
+    base_kw = {k.arg: k.value for k in base}
+    merged_kew = dict()
+    # TODO: fix this fuktion 
+    # def _get(ast_dict, key):
+    #     for k,v in ast_dict.items():
+    #         if compare(k,key):
+    #             return v
+    #     return None
+
+    for k, v in target_kw.items():
+        if k is None:
+            continue
+        assert isinstance(k, ast.Constant), f'dict key must be const {ast.dump(k)}'
+        keys_merged.append(k)
+        
+        if (same_value_base := base_kw.get( k)) is not None and \
+             isinstance(same_value_base, ast.Dict) and isinstance(v, ast.Dict):
+                # merge dict
+                merged_kew[k]=_merge_dict(v, same_value_base)
+        else:
+            # keep value 
+            merged_kew[k]=v
+    # add not merged base items
+    for k_base, v_base in base_key_value.items():
+        if k_base is None:
+            continue
+        if any((compare(k_base,s) for s in keys_merged ) ):
+            continue
+        else:
+            # not merged before and not in target
+            keys_merged.append(k_base)
+            values_merged.append(v_base)
+
+    return ast.Dict(keys =keys_merged,  values =values_merged)
+
+def _merge_assign_data_class(target: ast.Assign, base: ast.Assign) -> ast.Assign:
+    # check if both are expected dict type
+    # for s in (target, base):
+    #     assert isinstance(s, ast.Assign)
+    #     assert len(s.targets) == 1
+    #     assert isinstance(s.targets[0], ast.Name)
+    #     # assert isinstance(s.value, ast.Dict)
+    #
+    # assert isinstance(target.value, ast.Dict)
+    # assert isinstance(base.value, ast.Dict)
+    # target.value= _merge_call(target.value,base.value)
     return target
 
 def _is_dict_assign(stmt: ast.stmt) -> bool:
+    # TODO: Assign(targets=[Name(id='FOO3_attr', ctx=Store())], value=Call(func=Name(id='dict', ctx=Load()), args=[], keywords=[keyword(arg='name', value=Constant(value='foo'))]))
     return isinstance(stmt, ast.Assign) and isinstance(stmt.value, ast.Dict)
 
-def _is_dataclass_assign(assign: ast.Assign, imports: List[ast.stmt]) -> bool:
+def _is_dataclass_assign(assign: ast.Assign, imports: List[Union[ast.Import, ast.ImportFrom]]) -> bool:
     ast_m = ast.parse("from dataclasses import is_dataclass")
     is_dataclass_args =None
     # TODO:clean up
@@ -142,7 +202,6 @@ def _body_idx_after_last_import(target: ast.Module) -> int:
             return len(target.body)-i 
 
     return 0
-        
 
 
 def merge(target: ast.Module, base: ast.Module) -> ast.Module:
@@ -152,6 +211,8 @@ def merge(target: ast.Module, base: ast.Module) -> ast.Module:
     base_assigments_id_merged: List[str] = []
     fix_missing_locations_needed = False
     # all_import = get_imports(base).extend(target)
+    imports_base = get_imports(target)
+    imports_target = get_imports(base)
 
     for i, stm in enumerate(target.body):
         if not isinstance(stm,ast.Assign):
@@ -170,7 +231,8 @@ def merge(target: ast.Module, base: ast.Module) -> ast.Module:
                 # TODO: check im manipulation while iter is ok
                 AstAssinTransform(stm_merged).visit(target)
                 fix_missing_locations_needed = True
-            # elif _is_dict_assign(stm) and _is_dict_assign(same_base_assign):
+            elif _is_dataclass_assign(stm, imports_target) and _is_dataclass_assign(same_base_assign,imports_base):
+                assert False
 
     # add base imports to target at top via revered order, skip merged assignments
     # for stm_import in base_imports:
