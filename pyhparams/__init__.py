d2551d0972f4caaff8306c81231c5c99cad593fe
markus <markus.merklinger@bbraun.com>
2023-06-22 09:56:59 +0200

    rm import of not needed files

--- pyhparams/__init__.py
+++ pyhparams/__init__.py
@@ -1,10 +1,8 @@
 __version__=  '0.0.0dev'
 
-from .data_class import config_dataclass
-# private name for _PARAM_SUBSTITUTE from same user import -> ast same
-from .data_class import _PARAM_SUBSTITUTE as  PARAM_SUBSTITUTE
 from .config import Config
 from .utils import *
 from .ast_data_fields_resolve import RESOLVE
 
 __all__ =  ["RESOLVE", "Config","utils"]
+
